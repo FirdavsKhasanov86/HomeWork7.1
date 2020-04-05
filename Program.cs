8a6a925ecf6353bb01b9181f309f6a357f0d3959
user <user@Williams-iMac.local>
2020-04-05 18:20:34 +0500

    Added Constructor and object

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HomeWork7._1
     {
         static void Main(string[] args)
         {
-           
+           Converter con = new Converter();
         }
     }
 
@@ -16,5 +16,18 @@ namespace HomeWork7._1
         public double usd{get;set;}
         public double eur{get;set;}
         public double rub{get;set;}
+
+        public Converter(){
+            
+        }
+        public Converter(double converter, double usd, double eur, double rub){
+            this.converter = converter;
+            this.usd = usd;
+            this.eur = eur;
+            this.rub = rub;
+        } 
     }
+ 
+   
+   
 }
